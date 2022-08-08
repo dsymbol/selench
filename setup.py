5b31b264b23ef5448df22ef51084a22f3c107e83
dsymbol <dsymbol@users.noreply.github.com>
2022-08-08 15:49:31 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='selench',
-    version='1.2',
+    version='1.3',
     packages=['selench'],
     url='https://github.com/dsymbol/selench',
     license='OSI Approved :: MIT License',
     author='dsymbol',
     author_email='',
     platforms='OS Independent',
-    description='Python wrapper for selenium WebDriver',
+    description='Selenium WebDriver wrapper for Python',
     install_requires=['selenium'],
     classifiers=[
         "Programming Language :: Python :: 3",
